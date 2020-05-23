ec465cca2d020a18c058f313e0977986c30ceb4b
Matthew Law <61439842+Malaw97@users.noreply.github.com>
2020-05-23 16:09:44 -0400

    Initial commit

---
+++ Login Page/js/main.js
@@ -0,0 +1,19 @@
+const myform = document.getElementsByClassName('login100-form validate-form')[0];
+
+myform.addEventListener('submit', onsubmit);
+
+async function login(){
+    await wallet.requestSignIn(null, "near-api-js client-side challenge");
+}
+
+function onsubmit(button1){
+    button1.preventDefault();
+    if (wallet.isSignedIn()==true){
+        //Passthrough to next page
+        window.open('http://192.81.216.47:1234/')
+    }
+    else{
+        //Proceed to testnet login page
+        login()
+    }
+};

30a6a48065b9459472e5b524657fd4ba3786a16f
Matthew Law <61439842+Malaw97@users.noreply.github.com>
2020-05-23 19:39:19 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ async function login(){
 function onsubmit(button1){
     button1.preventDefault();
     if (wallet.isSignedIn()==true){
-        //Passthrough to next page.
+        //Passthrough to next page
         window.open('https://wallet.testnet.near.org/login/?title=NEAR+Guest+Book&contract_id=dev-1590275483536&success_url=http%3A%2F%2F192.81.216.47%3A1234%2F&failure_url=http%3A%2F%2F192.81.216.47%3A1234%2F&app_url=http%3A%2F%2F192.81.216.47%3A1234&public_key=ed25519%3A5WFr9DEJZAfkQ1Q1nxnJYu8GUBnMHN2Ko11oHDwndkHK')
     }
     else{
